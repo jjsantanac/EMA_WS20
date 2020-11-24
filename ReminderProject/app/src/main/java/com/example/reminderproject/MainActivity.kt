096caecdf7ab36352e4266cfd7fe79286304a0bf
Julian Felsch <inf3160@hs-worms.de>
2020-11-24 20:01:20 +0100

    Added notifications

--- ReminderProject/app/src/main/java/com/example/reminderproject/MainActivity.kt
+++ ReminderProject/app/src/main/java/com/example/reminderproject/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.reminderproject
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.content.Intent
+import android.os.Build
+import android.os.CountDownTimer
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
-    public lateinit var userinfo:settings.User
+    private val channel_id = "channel_id_example_01"
+    private val notificationId = 101
+
+    public lateinit var userinfo: settings.User
 
     private val switchActivity = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -16,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.profile -> {
                 val intent = Intent(this@MainActivity, settings::class.java)
-                intent.putExtra("user",userinfo)
+                intent.putExtra("user", userinfo)
                 startActivity(intent)
                 return@OnNavigationItemSelectedListener true
             }
@@ -39,14 +50,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userinfo= settings.User(name = "Julian")
+        userinfo = settings.User(name = "Julian")
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.botom_navigation)
-        bottomNavigation.selectedItemId=R.id.homescreen
+        bottomNavigation.selectedItemId = R.id.homescreen
 
         bottomNavigation.setOnNavigationItemSelectedListener(switchActivity)
 
+        createNotificationChannel()
+
+        val button_notify = findViewById<Button>(R.id.button_notification)
+        button_notify.setOnClickListener {
+            button_notify.text= "On"
+            val timer = object : CountDownTimer(10000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {}
+
+                override fun onFinish() {
+                    sendNotification()
+                    button_notify.text="Off"
+                }
+            }
+            timer.start()
+        }
 
 
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "MOVE THAT ASS"
+            val descriptionText = "Ich meins ernst du sau"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channel_id, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+    }
+
+    private fun sendNotification() {
+        val builder = NotificationCompat.Builder(this, channel_id)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Example Title")
+            .setContentText("Example Description")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
